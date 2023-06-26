3fedbd8e43e498617cdafaa11d7034008c0534f4
haf4545 <137810024+haf4545@users.noreply.github.com>
2023-06-26 18:53:39 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+// Пример функции, складывающей два числа
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Пример использования функции и вывод результата в консоль
+var num1 = 5;
+var num2 = 10;
+var sum = addNumbers(num1, num2);
+console.log("Сумма чисел " + num1 + " и " + num2 + " равна: " + sum);
